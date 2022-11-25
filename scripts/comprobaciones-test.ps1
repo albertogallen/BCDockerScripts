2651ee7e7f06b57ea35d20b9101dcabbc2227541
Alberto Gallen <alberto.gallen@gmail.com>
2022-11-25 16:05:03 +0100

    coolmod

---
+++ scripts/comprobaciones-test.ps1
@@ -0,0 +1,6 @@
+Get-BcContainerServerConfiguration -containerName bc1411es
+
+# Set-BcContainerServerConfiguration -keyName EnableSymbolLoadingAtServerStartup -keyValue true -containerName bc1411es
+
+#Restart-BcContainerServiceTier -ContainerName bc1411es
+#Import-TestToolkitToBcContainer -ContainerName BC1411ES -tenant default -testToolkitCountry es -useDevEndpoint
---
+++ scripts/creacion.ps1
@@ -0,0 +1,35 @@
+Import-Module bccontainerhelper
+
+$url = Get-BCArtifactUrl -type OnPrem -country nl 
+$cred = Get-Credential
+$sqlcred = Get-Credential
+$containerName = 'developal'
+
+$databaseParams = @{
+    "databaseServer"     = 'host.containerhelper.internal'
+    "databaseInstance"   = ''
+    "databasePrefix"     = "$containerName-"
+    "databaseName"       = 'CRONUS'
+    "databaseCredential" = $sqlcred 
+    "multitenant"        = $false
+}
+
+New-NavContainer @databaseParams `
+    -replaceExternalDatabases `
+    -accept_eula `
+    -accept_outdated `
+    -containerName $containerName `
+    -artifactUrl $url `
+    -locale 'nl-NL' `
+    -isolation 'process' `
+    -Credential $cred `
+    -auth UserPassword `
+    -updateHosts `
+    -useBestContainerOS `
+    -shortcuts desktop `
+    -licenseFile "C:\Users\gintautas\Documents\myData\7613755.flf" `
+    -includeTestToolkit
+
+
+
+Get-Bccontainerinfo -containername bc1411es | where-object { $_.name - like '*asser*' } | select-object -property name, version
